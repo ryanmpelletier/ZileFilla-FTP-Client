7227aff6df3a6af209c54ed4a541fb75d3d36371
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2016-03-22 08:39:39 -0400

    MainController will have references to local and remote file explorers

--- src/main/java/com/pelletier/controller/MainController.java
+++ src/main/java/com/pelletier/controller/MainController.java
@@ -4,8 +4,11 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+
 public class MainController {
     @FXML public LoginBar loginBar;
+//    @FXML public FileExplorer localFileExplorer;
+//    @FXML public FileExplorer remoteFileExplorer;
 
     public void exit(ActionEvent event){
         Platform.exit();
