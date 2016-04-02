4c498d8b9a6f57de84a540f1ff098f8a787b88db
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2016-04-01 22:26:37 -0400

    DirectoryViewManager is no longer an abstract class, because I can now inject a FileProvider.

--- src/main/java/com/pelletier/util/DirectoryViewManager.java
+++ src/main/java/com/pelletier/util/DirectoryViewManager.java
@@ -1,24 +1,76 @@
 package com.pelletier.util;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Created by ryanb on 3/31/2016.
  */
-public abstract class DirectoryViewManager {
+public class DirectoryViewManager {
 
     public TreeView<String> treeView;
     public TitledPane titledPane;
+    public String currentFilePath;
+    public FileItemProvider fileItemProvider;
 
-    public DirectoryViewManager(TitledPane titledPane, TreeView<String> treeView){
+    public DirectoryViewManager(TitledPane titledPane, TreeView<String> treeView, String currentFilePath, FileItemProvider fileItemProvider){
         this.titledPane = titledPane;
         this.treeView = treeView;
+        this.currentFilePath = currentFilePath;
+        this.fileItemProvider = fileItemProvider;
     }
 
-    //I would feel so much better if this returned a TreeView
-    public abstract void populateDirectoryView();
+    public void populateDirectoryView(){
+        TreeItem<String> root = new TreeItem<>(currentFilePath, new ImageView(new Image(getClass().getResourceAsStream("/images/folder.PNG"))));
+
+        treeView.getSelectionModel().selectedItemProperty().addListener((treeItem, oldValue, newValue) -> {
+            currentFilePath = buildCurrentFilePathFromTreeItem((TreeItem<String>) treeItem.getValue());   //it doesn't seem like this is updating the title pane
+            titledPane.setText("Local Site: " + currentFilePath);
+
+            if(fileItemProvider.children(currentFilePath) != null){
+                addTreeItems(treeItem.getValue(), currentFilePath);
+            }
+        });
+
+
+        treeView.setRoot(root);
+        addTreeItems(root, currentFilePath);
+        root.setExpanded(false);
+    }
+
+    private void addTreeItems(TreeItem<String> treeItem, String filePath){
+        treeItem.getChildren().remove(0, treeItem.getChildren().size());
+        for(String absoluteFilePath: fileItemProvider.children(currentFilePath)){   //may want to return absolute paths of children, not just their names
+            if(fileItemProvider.isDirectory(absoluteFilePath)){
+
+                TreeItem<String> directoryTreeItem = new TreeItem<>(fileItemProvider.getName(absoluteFilePath), new ImageView(new Image(getClass().getResourceAsStream("/images/folder.PNG"))));
+                directoryTreeItem.expandedProperty().addListener(new ChangeListener<Boolean>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                        BooleanProperty booleanProperty = (BooleanProperty) observable;
+                        TreeItem<String> t = (TreeItem<String>) booleanProperty.getBean();
+                        if(!newValue){
+                            t.getChildren().remove(0, t.getChildren().size());
+                            t.getChildren().add(new TreeItem<String>(""));
+                        }else{
+                            currentFilePath = buildCurrentFilePathFromTreeItem(t);  //it doesn't seem like this is updating the title pane
+                            addTreeItems(t, currentFilePath);
+                        }
+                    }
+                });
+                directoryTreeItem.getChildren().add(new TreeItem<>(""));
+                treeItem.getChildren().add(directoryTreeItem);
+            }else{
+                treeItem.getChildren().add(new TreeItem<>(fileItemProvider.getName(absoluteFilePath), new ImageView(new Image(getClass().getResourceAsStream("/images/file.PNG")))));
+            }
+        }
+    }
 
     //Gets a treeItem, follows its parents up the hierarchy, building a string for the absolute path
     //this is safe from either local or remote fileProvider, only uses tree items
